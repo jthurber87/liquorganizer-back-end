6d441ba188d861f08bcca824054f835400b824dc
Jay Thurber <jthurber87@gmail.com>
2021-10-01 17:04:26 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-// DEPENDENCIES
+//dep
+require('dotenv').config()
+require('./db/db')
 const express = require('express')
-// CONFIGURATION
+const cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = 9000
+const liquorganizer = require('./controllers/liquorganizer.js')
+require('./db/db')
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) != -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
 
-// 'DATA'
-const spirit = ['vodka', 'tequila', 'rum', 'whiskey', 'gin', 'beer', 'wine']
+// middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/bottles', liquorganizer)
 
-// ROUTES
-// index
-app.get('/bottles/', (req, res) => {
-  res.send(bottles)
-})
 
-// Listener
 app.listen(PORT, () => {
-  console.log('Popping bottles on ', PORT)
+  console.log(`Listening on port ${PORT}`)
 })
