0b9b0765ef232759108349d3efb1b47c7425209a
Jay Thurber <jthurber87@gmail.com>
2021-10-03 11:06:56 -0600

    Bottles model updated

--- models/bottle.js
+++ models/bottle.js
@@ -4,7 +4,7 @@ const bottleSchema = mongoose.Schema({
   spirit: {type: String, required: true},
   brand: {type: String, required: true},
   count: {type: Number, default: 1},
-  notes: {type: String}
+  notes: {type: String},
 })
 
 module.exports = mongoose.model('Bottle', bottleSchema)
