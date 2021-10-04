e51c938dcf5c5593f108d6538d7477fe9aba6d1a
Jay Thurber <jthurber87@gmail.com>
2021-10-04 16:05:09 -0600

    removed .env from gitignore that was breaking enviroment

--- db/db.js
+++ db/db.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
+const DB_URL = process.env.DB_URL || 'mongodb://localhost/'+ `liquorganizer
 
 //Error/DC
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
 
-mongoose.connect(process.env.DB_URL, {
+mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
