926e32402d0ee480ad6ffeafd61764d743e0e453
Jay Thurber <jthurber87@gmail.com>
2021-10-06 12:32:14 -0600

    adding bottles to heroku whitelist

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const PORT = process.env.PORT || 9000
-const whitelist = ["http://localhost:3000", "https://liquorganizer-ga.surge.sh/", "https://liquorganizer-back-end.herokuapp.com/"];
+const whitelist = ["http://localhost:3000", "https://liquorganizer-ga.surge.sh/", "https://liquorganizer-back-end.herokuapp.com/bottles"];
 const corsOptions = {
   origin: function(origin, callback) {
     if (whitelist.indexOf(origin) != -1 || !origin) {
